825862c9bfa7900c4d1a86d3e73da8a484e163f5
Rafael França <rafaelfms@gmail.com>
2018-04-11 16:23:16 -0300

    implementando mensagem para proprio usuario

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
@@ -6,8 +7,17 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.use(express.static(__dirname + '/public'));
+
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  socket.on('disconnect', () => console.log('user disconnected'));
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, () => {
